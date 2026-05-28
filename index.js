02320b52eefbdd5aa69660cb9c11e03cc6373154
Sourav RJ <souravjena@890.com>
2026-05-28 10:34:30 +0530

    Revert something

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const dob = '05/2005';
 function sayName() {
     console.log('My name is ' + name1)
 }
+<<<<<<< HEAD
  
 function newFn(){
     console.log('Hey I am a new fn')
+=======
+
+function isAbove18(){
+    return age >=18
+>>>>>>> parent of 321afee (Remove 18 function)
 }
